f9f0b778091de9f23b8461035deb17ce4165f99f
Tyler Elliott <tylercal@gmail.com>
2016-09-07 09:41:51 -0700

    look for remember text inside of labels as well

--- script.js
+++ script.js
@@ -11,9 +11,22 @@
 
         for (var i = 0; i < checkboxes.length && checks < MAX; i++) {
             var checkbox = checkboxes[i];
-            var text = (checkbox.name + checkbox.id).toLowerCase();
-            if (text.indexOf("remember") > -1 || text.indexOf("keep") > -1 || text.indexOf("persist") > -1) {
+            var boxText = (checkbox.name + checkbox.id).toLowerCase();
+            if (boxText.indexOf("remember") > -1 || boxText.indexOf("keep") > -1 || boxText.indexOf("persist") > -1) {
                 checkbox.checked = true;
+                checks++;
+            }
+        }
+
+        var labels = document.querySelectorAll("label");
+        checks = 0;
+
+        for (var j = 0; j < labels.length && checks < MAX; j++) {
+            var label = labels[j];
+            var labelText = (label.textContent).toLowerCase();
+            var subBox = label.querySelector('input[type=checkbox]');
+            if (subBox !== null && labelText.indexOf("remember") > -1 || labelText.indexOf("keep") > -1 || labelText.indexOf("persist") > -1) {
+                subBox.checked = true;
                 checks++;
             }
         }
